5d5946e5ce5d9614f98694614435e99d3a26fc6c
Sara Vieira <hey@iamsaravieira.com>
2021-01-24 16:21:20 +0100

    add oyther options

--- src/Components/AddEntry.js
+++ src/Components/AddEntry.js
@@ -1,8 +1,15 @@
 import { useState } from "react";
 
 const AddEntry = ({ onSubmit, data }) => {
-  const [form, setFrom] = useState({ name: "", description: "" });
-  const [url, setUrl] = useState([]);
+  const [form, setFrom] = useState({
+    name: "",
+    description: "",
+    url: "",
+    image: [],
+    sandbox: "",
+  });
+  const [showSandbox, setShowSandbox] = useState(false);
+  const [showUrl, setShowUrl] = useState(false);
 
   var myWidget = window.cloudinary.createUploadWidget(
     {
@@ -11,7 +18,10 @@ const AddEntry = ({ onSubmit, data }) => {
     },
     (error, result) => {
       if (!error && result && result.event === "success") {
-        setUrl((url) => [...url, result.info.url]);
+        setFrom((form) => ({
+          ...form,
+          image: [...form.image, result.info.url],
+        }));
       }
     }
   );
@@ -20,10 +30,7 @@ const AddEntry = ({ onSubmit, data }) => {
     e.preventDefault();
     const newItem = {
       ...data,
-      items: [
-        ...data.items,
-        { ...form, image: url, date: new Date().toString() },
-      ],
+      items: [...data.items, { ...form, date: new Date().toString() }],
     };
     await fetch(`${process.env.REACT_APP_API}${data.name}/${data._id}`, {
       headers: {
@@ -76,18 +83,79 @@ const AddEntry = ({ onSubmit, data }) => {
           />
         </div>
       </div>
-      {url.length ? (
+      {form.image.length ? (
         "File Uploaded"
       ) : (
-        <button
-          type="button"
-          onClick={() => myWidget.open()}
-          class="cloudinary-button"
-        >
-          Upload files
-        </button>
+        <span class="relative z-0 inline-flex shadow-sm rounded-md">
+          <button
+            onClick={() => myWidget.open()}
+            type="button"
+            class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          >
+            Image
+          </button>
+          <button
+            onClick={() => setShowUrl((url) => !url)}
+            type="button"
+            class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          >
+            URL
+          </button>
+          <button
+            onClick={() => setShowSandbox((sandbox) => !sandbox)}
+            type="button"
+            class="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
+          >
+            Sandbox ID
+          </button>
+        </span>
       )}
-
+      {showSandbox ? (
+        <div class="mb-2">
+          <label for="sandbox" class="block text-sm font-medium text-gray-700">
+            Sandbox ID
+          </label>
+          <div class="mt-1">
+            <input
+              value={form.sandbox}
+              onChange={(e) =>
+                setFrom((form) => ({
+                  ...form,
+                  sandbox: e.target.value,
+                }))
+              }
+              required
+              type="text"
+              name="sandbox"
+              id="sandbox"
+              class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+        </div>
+      ) : null}
+      {showUrl ? (
+        <div class="mb-2">
+          <label for="url" class="block text-sm font-medium text-gray-700">
+            URL
+          </label>
+          <div class="mt-1">
+            <input
+              value={form.url}
+              onChange={(e) =>
+                setFrom((form) => ({
+                  ...form,
+                  url: e.target.value,
+                }))
+              }
+              required
+              type="text"
+              name="url"
+              id="url"
+              class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+            />
+          </div>
+        </div>
+      ) : null}
       <button
         type="submit"
         class="flex items-center px-3 py-2 border mt-4 border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
--- src/Home.js
+++ src/Home.js
@@ -20,47 +20,52 @@ function App() {
   }, []);
 
   return (
-    <main class="max-w-6xl mx-auto sm:px-6 lg:px-20 pt-8 bg-gray-100 h-full">
-      <h1 class="mb-8">
-        <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
-          30 days of ...
-        </span>
-      </h1>
-      {challenges.length ? (
-        <ul class="space-y-3">
-          {challenges.map((ch) => (
-            <Link to={"/challenges/" + ch.name}>
-              <li class="bg-white shadow overflow-hidden rounded-md px-6 py-4 capitalize flex justify-between items-center mb-3">
-                {ch.name.split("-").join(" ")}
-
-                <div>
-                  <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium text-indigo-600">
-                    Started on {format(new Date(ch.date), "dd MMMM")}
+    <div class="bg-gray-100 ">
+      <main class="max-w-6xl mx-auto sm:px-6 lg:px-20 pt-8 h-full">
+        <h1 class="mb-8">
+          <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+            30 days of ...
+          </span>
+        </h1>
+        {challenges.length ? (
+          <ul class="space-y-3">
+            {challenges.map((ch) => (
+              <Link to={"/challenges/" + ch.name}>
+                <li class="bg-white shadow overflow-hidden rounded-md px-6 py-4  flex justify-between items-center mb-3">
+                  <span class="capitalize">
+                    {" "}
+                    {ch.name.split("-").join(" ")}
                   </span>
-                  <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800">
-                    {ch.items.length} entr{ch.items.length === 1 ? "y" : "ies"}
-                  </span>
-                </div>
-              </li>
-            </Link>
-          ))}
-        </ul>
-      ) : (
-        <h2 className="text-center text-gray-800">
-          You have no challenges, start one
-        </h2>
-      )}
-      {isAdmin() && (
-        <button
-          onClick={() => setAddChallenge(true)}
-          type="button"
-          class="m-auto mt-6 flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Add new challenge
-        </button>
-      )}
-      {addChallenge ? <AddChallengeForm /> : null}
-    </main>
+                  <div>
+                    <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium text-indigo-600">
+                      Started on {format(new Date(ch.date), "dd MMMM")}
+                    </span>
+                    <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800">
+                      {ch.items.length} Entr
+                      {ch.items.length === 1 ? "y" : "ies"}
+                    </span>
+                  </div>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          <h2 className="text-center text-gray-800">
+            You have no challenges, start one
+          </h2>
+        )}
+        {isAdmin() && (
+          <button
+            onClick={() => setAddChallenge(true)}
+            type="button"
+            class="m-auto mt-6 flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Add new challenge
+          </button>
+        )}
+        {addChallenge ? <AddChallengeForm /> : null}
+      </main>
+    </div>
   );
 }
 
